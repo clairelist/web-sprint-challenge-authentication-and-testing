18cd6d555db2caf9aa29670025e380f1a663fabc
Claire List <claire.list2501@gmail.com>
2022-01-27 21:15:17 -0700

    outline of dads model

---
+++ api/auth/dads-model.js
@@ -0,0 +1,7 @@
+//include db from db config !
+//build getAll functionality
+//build getById for callback of below
+//build getByFilter for calling getById for checking existing username existy
+//build create which checks for existing user, then returns that user if not existy before !
+//we are only using one table, so we don't need to use a transaction here.
+//it will be simple :)
