efd7077da80925003852f77232a087bcd3871ff8
Claire List <claire.list2501@gmail.com>
2022-01-28 10:15:52 -0700

    testing is fun

--- api/server.test.js
+++ api/server.test.js
@@ -1,6 +1,23 @@
 // Write your tests here
+/* eslint-disable */
+const request = require('supertest')
+const server = require('./api/server')
+const db = require('./data/db-config')
+const bcrypt = require('bcryptjs')
+const jwtDecode = require('jwt-decode')
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+})
+beforeEach(async () => {
+  await db.seed.run();
+})
+afterAll(async () => {
+  await db.destroy();
+})
 test('sanity', () => {
-  expect(true).toBe(true)
+  expect(true).toBe(true);
 })
 
 
