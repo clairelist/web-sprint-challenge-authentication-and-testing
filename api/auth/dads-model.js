9e8285c961629bc3ea7a1cbd74a8cc1459bccb8f
Claire List <claire.list2501@gmail.com>
2022-01-27 21:25:00 -0700

    more notes to dads model

--- api/auth/dads-model.js
+++ api/auth/dads-model.js
@@ -1,6 +1,12 @@
 //include db from db config !
+const lego = require('../../data/dbConfig.js');
 //build getAll functionality
+//before we build these, be SURE TO RUN MIGRATION ON THE DATABASE AND OPEN IT IN SQLITE !!!!!
+//table 'users', select * from users
+
 //build getById for callback of below
+//select * from users where id = user_id
+
 //build getByFilter for calling getById for checking existing username existy
 //build create which checks for existing user, then returns that user if not existy before !
 //we are only using one table, so we don't need to use a transaction here.
