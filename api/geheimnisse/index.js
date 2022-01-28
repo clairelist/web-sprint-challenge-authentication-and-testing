d0d7b4cfd5f131596a083a4285e7b4fb754202eb
Claire List <claire.list2501@gmail.com>
2022-01-27 21:18:40 -0700

    geheimnisse bedeutet secret

--- api/geheimnisse/index.js
+++ api/geheimnisse/index.js
@@ -1,4 +1,5 @@
 //this handles the JWT secret !
+//obfuscation:: they can't find the secret if they don't know the language the word is written in ;)
 module.exports = {
     JWT_SECRET : process.env.JWT_SECRET || 'shh'
   }
